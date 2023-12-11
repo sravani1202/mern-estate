0ed950512d106f8da05f0e4c4b0738ee77cd7e69
keerthisravani <keerthisravani1202@gmail.com>
2023-12-11 16:08:33 +0530

    done

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv';
+// import userRouter from './routes/user.route.js';
+dotenv.config();
+mongoose.connect("mongodb+srv://shann:shann@mern-estates.cualgwh.mongodb.net/mern-estates?retryWrites=true&w=majority")
+.then( () =>
+{
+console.log('Connected to MongoDb!');
+}
+) 
+.catch( err => {
+    console.log(err);
+})
+const app=express();
+app.listen(3000, () =>{
+    console.log('Server is running on port 3000');
+}); 
+//app.use("/api/user",userRouter);
