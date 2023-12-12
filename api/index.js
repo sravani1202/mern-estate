2afe88e5ab4b673adc7c4291f148bbadd15f829d
keerthisravani <keerthisravani1202@gmail.com>
2023-12-12 09:56:11 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose'; 
 import dotenv from 'dotenv';
-// import userRouter from './routes/user.route.js';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect("mongodb+srv://shann:shann@mern-estates.cualgwh.mongodb.net/mern-estates?retryWrites=true&w=majority")
 .then( () =>
@@ -16,4 +16,6 @@ const app=express();
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
 }); 
-//app.use("/api/user",userRouter);
+
+
+app.use("/api/user",userRouter);
