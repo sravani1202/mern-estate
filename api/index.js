266056f4dfcfe83b35f0cb8c0e492333dfbcc185
keerthisravani <keerthisravani1202@gmail.com>
2023-12-15 16:20:49 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose'; 
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
+import userRouter from './routes/user.route.js'; 
+import authRouter from './routes/auth.route.js';
+//import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect("mongodb+srv://shann:shann@mern-estates.cualgwh.mongodb.net/mern-estates?retryWrites=true&w=majority")
 .then( () =>
@@ -13,9 +15,14 @@ console.log('Connected to MongoDb!');
     console.log(err);
 })
 const app=express();
+
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
 }); 
 
 
-app.use("/api/user",userRouter);
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+//app.use('/api/auth',authRouter);
